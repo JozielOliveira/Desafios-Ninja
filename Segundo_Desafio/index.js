197a3c83ef9ebc653c3610afd0a41f8572a2c4d5
Joziel Oliveira <programdor.joziel@gmail.com>
2018-03-07 11:40:54 -0300

    Atualizando o Desafio

--- Segundo_Desafio/index.js
+++ Segundo_Desafio/index.js
@@ -2,7 +2,7 @@ function alerta() {
     
     let texto = document.getElementById('texto').value;
 
-    alert(`${texto}, 2+2= ${2+2}
+    alert(`${texto}, 322+2= ${2+2}
     O DOM é a interface por onde os programas e scripts pode acessar e atualizar dinamicamente os conteúdos do HTML ou do CSS`)
 
 }
