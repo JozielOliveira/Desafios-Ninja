c465c8696fcebe9eecb8525691d6b35d3472fc43
Joziel Oliveira <programdor.joziel@gmail.com>
2018-03-08 01:10:11 -0300

    adicionei o desafio 4

---
+++ Quarto_Desafio/index.js
@@ -0,0 +1,32 @@
+let api = "https://pacific-earth-39167.herokuapp.com/codigo/"
+var latitude
+var longitude
+
+    axios.get(`${api}panqueca`)
+    .then(function (response) {
+        latitude = response.data.cidade.lat
+        longitude = response.data.cidade.long
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+    
+longitude = parseInt(longitude)
+latitude  = parseInt(latitude)
+
+function initMap() {
+    
+    let centro = {lat:  latitude, lng: longitude};
+    
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: centro
+    });
+    
+    let marker = new google.maps.Marker({
+      position: centro,
+      map: map
+    });
+
+    document.getElementById("ll").innerHTML = `Latitude : ${latitude}, Longitude : ${longitude}`
+}
